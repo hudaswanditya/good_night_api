059a074a74fa1405d45134866d3b354cefa6f34b
hudaswanditya <huda.swanditya@gmail.com>
2025-03-10 21:48:21 +0700

    try optimizing db and server

--- config/puma.rb
+++ config/puma.rb
@@ -39,3 +39,8 @@ plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
 # Specify the PID file. Defaults to tmp/pids/server.pid in development.
 # In other environments, only set the PID file if requested.
 pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
+
+workers ENV.fetch("WEB_CONCURRENCY") { 6 }  # Increase workers from 4 → 6
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 32 }
+threads threads_count, threads_count  # Handle 2x more traffic
+preload_app!
