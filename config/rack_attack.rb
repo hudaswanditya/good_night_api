6c0b5f43901498f4f7e49680a52fc432416c6262
hudaswanditya <huda.swanditya@gmail.com>
2025-03-11 10:07:31 +0700

    environment configuration

--- config/rack_attack.rb
+++ config/rack_attack.rb
@@ -1,6 +1,11 @@
 class Rack::Attack
-  # Limit API requests to 60 per minute per user
-  throttle("api/ip", limit: 60, period: 1.minute) do |req|
-    req.ip
+  throttle("limit sleep requests", limit: 10, period: 60.seconds) do |req|
+    if req.path.match?(/\/api\/v1\/users\/\d+\/sleep_records\/(start|stop)/)
+      req.ip # Rate limit by IP
+    end
+  end
+
+  self.throttled_response = lambda do |_env|
+    [ 429, { "Content-Type" => "application/json" }, [ { status: "error", message: "Too many requests. Slow down!" }.to_json ] ]
   end
 end
